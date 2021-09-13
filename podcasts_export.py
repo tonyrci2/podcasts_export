01cb8a45f43784dd0b79261409a4ce0c33280c24
Tony J <79281146+tonyrci2@users.noreply.github.com>
2021-09-12 20:11:40 -0400

    copy 'description' to 'comment' field
    copies podcast description (ZITUNESSUBTITLE) to comment field of the ID3 tag. due to tag length limit, it only copies first 255 characters.

--- podcasts_export.py
+++ podcasts_export.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-#
 #  Podcasts Export
 #  ---------------
 #  Douglas Watson, 2020, MIT License
@@ -17,7 +15,7 @@ import urllib.parse
 import sqlite3
 
 SQL = """
-SELECT p.ZAUTHOR, p.ZTITLE, e.ZTITLE, e.ZASSETURL
+SELECT p.ZAUTHOR, p.ZTITLE, e.ZTITLE, e.ZITUNESSUBTITLE, e.ZASSETURL
 from ZMTEPISODE e 
 join ZMTPODCAST p
     on e.ZPODCASTUUID = p.ZUUID 
@@ -41,7 +39,7 @@ def get_downloaded_episodes(db_path):
 def main(db_path, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    for author, podcast, title, path in get_downloaded_episodes(db_path):
+    for author, podcast, title, description, path in get_downloaded_episodes(db_path):
         safe_title = title.replace('/', '|').replace(':', ',')
         safe_podcast = podcast.replace('/', '|').replace(':', ',')
         safe_author = author.replace('/', '|').replace(':', ',')
@@ -53,14 +51,41 @@ def main(db_path, output_dir):
         try:
             mp3 = MP3(dest_path, ID3=EasyID3)
         except HeaderNotFoundError:
-            print(u"Corrupted file: {} - {}".format(podcast, title))
+            print(u"Failed to export {} - {}: Corrupted file.".format(podcast, title))
             continue
         if mp3.tags is None:
             mp3.add_tags()
         mp3.tags['artist'] = author
         mp3.tags['album'] = podcast
         mp3.tags['title'] = title
+        # Strip non-ascii characters and set - want to use non-ascii as well, so will construct tag object 'manually'
+        # mp3.tags['comment'] = [description.encode('ascii',errors='ignore').decode('ascii')[:255]]
         mp3.save()
+        # Tweak Comment Tag so it's visible in iTunes/Music
+        ftag = ID3(dest_path)
+        """
+        #comment_key_name = next(x for x in ftag.keys() if x.startswith('COMM'))
+        comment_key = ftag.getall('COMM')[0]
+        comment_key_name = f"{comment_key.FrameID}::{comment_key.lang}"
+        # Encoding to LATIN1, default is UTF8
+        ftag[comment_key_name].encoding=0 
+        # Language to English, default is XXX
+        ftag[comment_key_name].lang='eng'
+        """
+        # remove all comment tags just in case
+        '''
+        # check comment tags
+        ftag.getall('COMM')
+        '''
+        ftag.delall('COMM')
+
+        #construct comment object
+        new_comm = COMM(encoding=Encoding.UTF16, lang='eng', text=[description[:255]])
+        ftag.add(new_comm)
+
+        # save file again to fix ID3 tag
+        ftag.save()
+
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
@@ -73,5 +98,10 @@ if __name__ == "__main__":
     check_imports()
     from mutagen.mp3 import MP3, HeaderNotFoundError
     from mutagen.easyid3 import EasyID3
-
+    '''
+    # Register comment tag - produces broken comment that does not display in iTunes/Music, will construct manually instead
+    EasyID3.RegisterTextKey('comment', 'COMM')
+    '''
+    # Instead, Import ID3 Class for Post-Edits
+    from mutagen.id3 import ID3,COMM,Encoding
     main(db_path, output_dir)
